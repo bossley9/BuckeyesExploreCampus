393aaf3a14c29685696d58d2759c234aedf2d913
Nick Shaughnessy <nshaughnessy21@gmail.com>
2020-03-04 17:14:02 -0500

    Implements basic CRUD functionality with Firestore

--- app/src/main/java/com/example/buckeyesexplorecampus/MainActivity.kt
+++ app/src/main/java/com/example/buckeyesexplorecampus/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
